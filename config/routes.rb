f2af19d42ee230b10b91262983e6c51c3009d89d
Junya Kemmochi <github@tsurugeek.com>
2018-12-21 20:35:38 +0900

    deviseのログイン等ができるところまで。リダイレクト先などは他の画面を作ったら着手する

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "home#show"
+
+  # TODO: adminsはnamespace: admin配下に持っていきたい。今はページがなくてやりづらいのであとで変更する。
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords'
+  }
+
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  namespace :admin do
+    get 'home', to: 'home#show'
+  end
+
+  # TODO: これは仮に作ったのであとでコントローラごと消す
+  get 'home', to: 'home#show'
+
+
 end
