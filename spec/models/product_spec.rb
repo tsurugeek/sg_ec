024c1f0a18b6a731163eca46349a6270c3083c9f
Junya Kemmochi <github@tsurugeek.com>
2018-12-27 18:22:35 +0900

    productモデルのspecを追加

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:product){create(:product)}
+
+  describe "about validation" do
+    it "空のnameは異常" do
+      product = Product.new(name: nil)
+      product.valid?
+      expect(product.errors[:name].size).to be > 0
+    end
+
+    context "active == true の時" do
+      it "空のpriceまたはsort_noは異常" do
+        product.active = true
+        product.price = nil
+        product.sort_no = nil
+        product.valid?
+        expect(product.errors[:price].size).to be > 0
+        expect(product.errors[:sort_no].size).to be > 0
+      end
+    end
+
+    context "active == false の時" do
+      it "空のpriceまたはsort_noは正常" do
+        product.active = false
+        product.price = nil
+        product.sort_no = nil
+        product.valid?
+        expect(product.errors[:price].size).to be 0
+        expect(product.errors[:sort_no].size).to be 0
+      end
+    end
+  end
 end
