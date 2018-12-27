119e50da548fcf20bd61a361d60ea375a376ca7a
Junya Kemmochi <github@tsurugeek.com>
2018-12-27 17:47:43 +0900

    ナビゲーションバーのヘルパー作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
-  def nav_active?(name)
-    controller_name == name
+  # def nav_active?(name)
+  #   controller_name == name
+  # end
+
+  def nav_item item_name, path, active_controller_name
+    is_active = active_controller_name == controller_name
+
+    tag.li class: "nav-item #{"active" if is_active}" do |li|
+      concat li.a(item_name, href: path, class: "nav-link")
+      concat li.span("(current)", class: "sr-only") if is_active
+    end
   end
 end
