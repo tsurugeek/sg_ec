f904bf94a377a8f9e80768cfb6993ccc3cb79dcd
Junya Kemmochi <github@tsurugeek.com>
2018-12-27 16:59:44 +0900

    sequenceで書き換えた

--- spec/factories/shipping_addresses.rb
+++ spec/factories/shipping_addresses.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :shipping_address do
-    user_id { 1 }
-    name { "MyString" }
-    postal_code { "MyString" }
-    prefecture { "MyString" }
-    city { "MyString" }
-    address { "MyString" }
-    building { "MyString" }
+    sequence(:name) {|n| "shipping_address name #{n}"}
+    postal_code { "000-0000" }
+    sequence(:prefecture) {|n| "shipping_address prefecture #{n}"}
+    sequence(:city) {|n| "shipping_address city #{n}"}
+    sequence(:address) {|n| "shipping_address address #{n}"}
+    sequence(:building) {|n| "shipping_address building #{n}"}
   end
 end
