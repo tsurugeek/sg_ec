99b1c57c4f329580cb80615a335353f66c9eebbb
Junya Kemmochi <github@tsurugeek.com>
2019-01-02 16:37:36 +0900

    不要なコードを削除

--- app/controllers/user/application_controller.rb
+++ app/controllers/user/application_controller.rb
@@ -8,15 +8,4 @@ class User::ApplicationController < ApplicationController
   def store_user_location!
     store_location_for(:user, request.fullpath)
   end
-
-  def after_sign_out_path_for(resource_or_scope)
-    logger.debug{ "after_sign_out_path_for resource_or_scope=#{resource_or_scope}"}
-    # オーバーライドしているメソッドの引数名は resource_or_scope となっているが、その説明文にはシンボルしか渡されない、と書いてある。
-    # "this method receives a symbol with the scope, and not the resource"
-    if resource_or_scope == :admin
-      new_admin_session_path
-    else
-      root_path
-    end
-  end
 end
