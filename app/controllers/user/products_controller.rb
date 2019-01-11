8d81b0d1af5a88ddfa84693cf220fca60c39ad05
Junya Kemmochi <github@tsurugeek.com>
2019-01-11 16:52:32 +0900

    商品ページにページネーションを追加

--- app/controllers/user/products_controller.rb
+++ app/controllers/user/products_controller.rb
@@ -2,7 +2,7 @@ class User::ProductsController < User::ApplicationController
   before_action :set_product, only: [:show]
 
   def index
-    @products = Product.published.order(:sort_no).all
+    @products = Product.published.order(:sort_no).page(params[:page]).per(20).all
   end
 
   def show
